82050b13fc6f97570cc3d98a63f511aa0ceb63d1
kunatastic <kjha6666@gmail.com>
2021-01-15 23:56:29 +0530

    author routes made

---
+++ routes/author.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+const Author = require("../Models/author");
+
+// All the authors route
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors,
+      searchOptions: req.query,
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+// New author Route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create authors route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`author/${newAuthor.id}`);
+    res.redirect("authors");
+  } catch (error) {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
+});
+
+module.exports = router;
